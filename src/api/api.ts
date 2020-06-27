423c8d79f6e82f575f8a09082c293d35df196884
Leon Erath <leon-erath@hotmail.de>
2020-06-27 10:50:38 +0200

    feat: add dev environment

--- src/api/api.ts
+++ src/api/api.ts
@@ -7,7 +7,7 @@ import { Course, SubCourse, Lecture, CourseOverview } from '../types/Course';
 const dev = process.env.NODE_ENV === 'development';
 
 const apiURL = dev
-  ? 'http://localhost:5000/api'
+  ? 'https://dev.api.corona-school.de/api/'
   : `https://api.corona-school.de/api`;
 
 export const redeemVerificationToken = (
