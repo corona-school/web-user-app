5be168f958281374a62557d45da3d786d6cfc92a
Leon Erath <leon-erath@hotmail.de>
2020-06-28 20:32:37 +0200

    fix: no env probblems

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,11 @@
 export const dev = process.env.NODE_ENV === 'development';
 
-export const apiURL = dev
+const url = dev
   ? process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000/api'
   : process.env.REACT_APP_BACKEND_URL || 'https://api.corona-school.de/api';
+
+console.log(url);
+
+(window as any).apiURL = url;
+
+export const apiURL = (window as any).apiURL;
