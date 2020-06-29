b93b36171186eb5c2354eea29b69a20c331bf36a
Leon Erath <leon-erath@hotmail.de>
2020-06-29 21:48:35 +0200

    idk

--- server.js
+++ server.js
@@ -5,15 +5,14 @@ const port = process.env.PORT || 8080;
 const app = express();
 require('dotenv').config();
 const envs = require('./env.js').default;
-
-app.use(favicon(__dirname + '/build/favicon.ico'));
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-
 app.get('/env.js', function (req, res) {
   res.set('Content-Type', 'application/javascript');
   res.send(envs);
 });
+
+app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/ping', function (req, res) {
   return res.send('pong');
