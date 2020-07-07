c2c96ab8aedb861b9b937cf3fc79ec22b0a683ca
Leon Erath <leon-erath@hotmail.de>
2020-07-07 21:06:14 +0200

    fix: remove localhost

--- src/api/config.ts
+++ src/api/config.ts
@@ -4,8 +4,7 @@ const liveDomains = [
   'web-user-app-live.herokuapp.com',
 ];
 
-const devAPI =
-  'http://localhost:5000/api' || 'https://dev.api.corona-school.de/api';
+const devAPI = 'https://dev.api.corona-school.de/api';
 
 export const apiURL = liveDomains.includes(window.location.host)
   ? 'https://api.corona-school.de/api'
