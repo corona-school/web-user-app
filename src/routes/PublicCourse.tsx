db6580965d0c6253b43816e9a1f529d6e93356ac
Lennard Schiefelbein <lennard@familie-schiefelbein.de>
2020-07-23 21:24:44 +0200

    Add header to Course Overview

--- src/routes/PublicCourse.tsx
+++ src/routes/PublicCourse.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import CourseOverview from "../components/CourseOverview";
+import classes from "./PublicCourse.module.scss";
+import Icons from "../assets/icons";
+import {Title} from "../components/Typography";
 
 const PublicCourse = () => {
+  const Header = () => {
+    return (
+      <div className={classes.header}>
+        <div className={classes.logo}>
+          <Icons.Logo />
+          <Title className={classes.logoText} >
+            Corona School
+          </Title>
+        </div>
+        <Icons.WirVsVirusLogo />
+      </div>
+    )
+  }
+
   return (
-    <CourseOverview />
+    <div className={classes.container}>
+      <Header />
+      <CourseOverview />
+    </div>
   )
 }
 
