9b88357a39c528d961d170bc630fbc17bd571ad3
Leon Erath <leon-erath@hotmail.de>
2020-08-16 10:07:16 +0200

    fix #62

--- src/api/config.ts
+++ src/api/config.ts
@@ -4,8 +4,10 @@ const liveDomains = [
   'web-user-app-live.herokuapp.com',
 ];
 
-const devAPI = 'https://dev.api.corona-school.de/api';
-// const devAPI = 'http://localhost:5000/api';
+const devAPI =
+  process.env.REACT_APP_MODE === 'LOCAL'
+    ? 'http://localhost:5000/api'
+    : 'https://dev.api.corona-school.de/api';
 
 export const apiURL = liveDomains.includes(window.location.host)
   ? 'https://api.corona-school.de/api'
