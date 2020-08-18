389ca9830cb9367c799c0bc6af29c6359adfab5f
Gero <gero.embser@gmail.com>
2020-08-18 12:13:26 +0200

    Fix course join problem
    Some courses weren’t joinable at some times. This is probably caused by this wrong date handling of unix times. This commit should fix this.

--- src/routes/CourseDetail.tsx
+++ src/routes/CourseDetail.tsx
@@ -261,8 +261,8 @@ const CourseDetail = (params: { id?: string }) => {
       return false;
     }
 
-    const hasCourseStarted = course.subcourse.lectures.some(
-      (l) => new Date().getDate() - l.start < 0
+    const hasCourseStarted = course.subcourse.lectures.some((l) =>
+      moment.unix(l.start).isBefore(moment())
     );
     if (!course.subcourse.joinAfterStart && hasCourseStarted) {
       return false;
