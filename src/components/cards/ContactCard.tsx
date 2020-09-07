cd6b1692fbfe0acbf5c642e04285245a8c8c6248
Lennard Schiefelbein <lennard@familie-schiefelbein.de>
2020-09-07 23:12:45 +0200

    Optimize contact card

--- src/components/cards/ContactCard.tsx
+++ src/components/cards/ContactCard.tsx
@@ -3,6 +3,7 @@ import styled, { ThemeContext } from 'styled-components';
 import { Select, Input } from 'antd';
 import { Title } from '../Typography';
 import classes from './ContactCard.module.scss';
+import Button from '../button/index';
 
 const { TextArea } = Input;
 
@@ -13,25 +14,39 @@ const Highlight = styled.div<{ highlightColor: string }>`
 `;
 
 export const ContactCard = (props) => {
-  const theme = useContext(ThemeContext);
+  const cardColor = useContext(ThemeContext).color.cardHighlightRed;
   const [recipient, setRecipient] = useState(props.categories[0].value);
   const [message, setMessage] = useState('');
 
+  const submit = () => {
+    console.log(`Send ${message} to ${recipient}`);
+  };
+
   return (
     <div className={classes.container}>
-      <Highlight highlightColor={theme.color.cardHighlightRed} />
-      <Title size="h4">Du hast Fragen?</Title>
-      <Select
-        value={recipient}
-        onChange={(value) => setRecipient(value)}
-        options={props.categories}
-        style={{ width: 500 }}
-      />
-      <TextArea
-        rows={4}
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
+      <Highlight highlightColor={cardColor} />
+      <div className={classes.content}>
+        <Title size="h4" style={{ marginLeft: '0px' }}>
+          Du hast Fragen?
+        </Title>
+        <div className={classes.categorySelect}>
+          {'Kategorie: '}
+          <Select
+            value={recipient}
+            onChange={(value) => setRecipient(value)}
+            options={props.categories}
+            style={{ marginLeft: 10, minWidth: 430 }}
+          />
+        </div>
+        <TextArea
+          rows={4}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <Button color="white" backgroundColor={cardColor} onClick={submit}>
+          Abschicken
+        </Button>
+      </div>
     </div>
   );
 };
