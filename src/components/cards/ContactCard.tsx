bbcbff9a09274958f567d3a7b92aca3330093016
Lennard Schiefelbein <lennard@familie-schiefelbein.de>
2020-09-26 02:48:10 +0200

    Improve mentoring page layout

--- src/components/cards/ContactCard.tsx
+++ src/components/cards/ContactCard.tsx
@@ -11,16 +11,17 @@ import { ApiContext } from '../../context/ApiContext';
 import { TopHighlightCard } from './FlexibleHighlightCard';
 
 const SelectStyle = styled.select`
-  width: 310px;
   height: 28px;
   padding: 2px 5px;
   border: 1px solid ${(props) => props.theme.colorScheme.gray1};
-  box-sizing: border-box;
   font-size: 15px;
   line-height: 22px;
   letter-spacing: -0.333333px;
   color: border-color: rgb(244, 72, 109);
-  border-color: rgb(244, 72, 109)
+  border-color: rgb(244, 72, 109);
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
 `;
 
 enum FormStates {
@@ -77,14 +78,15 @@ const ContactCard = () => {
         </div>
         <div className={classes.categorySelect}>
           <Text large> Kategorie: </Text>
-          <SelectStyle
+          <select
+            className={classes.selectElement}
             onChange={(e) => setCategory(MentoringCategory[e.target.value])}
             value={category}
           >
             {Object.values(MentoringCategory).map((c) => (
               <option value={c}>{messageLabels.get(c)}</option>
             ))}
-          </SelectStyle>
+          </select>
         </div>
         <div className={classes.image}>
           <Images.MentoringPic />
@@ -96,6 +98,7 @@ const ContactCard = () => {
             className={classes.inputField}
             onChange={(e) => setMessage(e.target.value)}
             value={message}
+            rows={10}
           />
         </div>
         <div className={classes.checkboxCell}>
