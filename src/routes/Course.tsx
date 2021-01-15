2cf561862612ded7fe63fdd9af77e9f69be2918f
realmayus <realmayus@gmail.com>
2021-01-15 17:03:16 +0100

    Replaced current course overview with the same carousel that is being used on the "find new courses" page

--- src/routes/Course.tsx
+++ src/routes/Course.tsx
@@ -1,18 +1,14 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { Empty } from 'antd';
 import Context from '../context';
-import { Title } from '../components/Typography';
 import { LinkButton } from '../components/button';
 import Icons from '../assets/icons';
 import { ParsedCourseOverview } from '../types/Course';
-import MyCourseCard from '../components/cards/MyCourseCard';
 
 import classes from './Course.module.scss';
 import { parseCourse } from '../utils/CourseUtil';
 import { UserContext } from '../context/UserContext';
 import { CourseBanner } from '../components/course/CourseBanner';
-import { env } from '../api/config';
-import { NoCourses } from '../components/NoService';
+import { CourseList } from '../components/course/CourseList';
 
 const Course = () => {
   const [loading, setLoading] = useState(false);
@@ -20,11 +16,7 @@ const Course = () => {
   const apiContext = useContext(Context.Api);
   const userContext = useContext(UserContext);
 
-  const courseOverviewDisabled = env.REACT_APP_COURSE_OVERVIEW === 'disabled';
-
   useEffect(() => {
-    if (courseOverviewDisabled) return;
-
     setLoading(true);
 
     apiContext
@@ -40,10 +32,6 @@ const Course = () => {
       });
   }, [apiContext, userContext.user.type]);
 
-  if (courseOverviewDisabled) {
-    return <NoCourses />;
-  }
-
   if (loading) {
     return <div>Kurse werden geladen...</div>;
   }
@@ -57,7 +45,6 @@ const Course = () => {
       />
       <div className={classes.containerRequests}>
         <div className={classes.header}>
-          <Title size="h1">Deine Kurse</Title>
           {userContext.user.type === 'student' && (
             <LinkButton
               href="/courses/create"
@@ -71,15 +58,9 @@ const Course = () => {
             </LinkButton>
           )}
         </div>
-        <div className={classes.myCoursesContainer}>
-          {myCourses.length === 0 ? (
-            <Empty description="Du hast im Moment keine Kurse" />
-          ) : (
-            myCourses.map((c) => {
-              return <MyCourseCard course={c} ownedByMe showCourseState />;
-            })
-          )}
-        </div>
+        {myCourses.length > 0 && (
+          <CourseList name="Deine aktuellen Kurse" courses={myCourses} />
+        )}
       </div>
     </div>
   );
