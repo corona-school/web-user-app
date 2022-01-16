64a52242725bcb5de74e3bcefddb9eb30bbf9e4a
Lennard Schiefelbein <lennard@familie-schiefelbein.de>
2022-01-16 23:33:38 +0100

    Edit CoDu introduction text

--- src/assets/coDuAssets.tsx
+++ src/assets/coDuAssets.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
-export const introductionText =
-  'Sei Teil der CoDu-Studie! Zusammen mit dem Institut für Arbeitsmarkt und ' +
-  'Berufsforschung (IAB) untersucht Lern-Fair, inwiefern Lernrückstände ' +
-  'durch eine online-basierte Lernunterstützung bei Schüler:innen aus einem ' +
-  'sozial schwachen Umfeld reduziert werden können. Für deine dreimonatigen ' +
-  'Lernunterstützung im Rahmen der Studie erhältst du einen 50€ Gutschein. ' +
-  'Mit der Teilnahme an der Studie stimmst du einer Weitergabe deiner ' +
-  'anonymisierten Daten an das IAB, welches die Datenauswertung vornimmt, ' +
-  'zu. Weitere Infos findest du hier [links to pdf].';
+export const introductionText = (
+  <>
+    Sei Teil der CoDu-Studie! Zusammen mit dem Institut für Arbeitsmarkt und
+    Berufsforschung (IAB) untersucht Lern-Fair, inwiefern Lernrückstände durch
+    eine online-basierte Lernunterstützung bei Schüler:innen aus einem sozial
+    schwachen Umfeld reduziert werden können. Für deine dreimonatige
+    Lernunterstützung im Rahmen der Studie erhältst du einen 50€ Gutschein. Mit
+    der Teilnahme an der Studie stimmst du einer Weitergabe deiner
+    anonymisierten Daten an das IAB, welches die Datenauswertung vornimmt, zu.
+    Weitere Infos findest du{' '}
+    <a href="https://www.lern-fair.de/codu-studie">hier</a>.
+  </>
+);
 
 export const subjectErrorText = (
   <>
