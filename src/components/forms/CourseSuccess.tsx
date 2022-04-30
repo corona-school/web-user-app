61f952750c0af33b3118e8f891fc89f4a0a45df6
Jonas Wilms <JonasWilms2000@gmail.com>
2022-05-01 00:27:38 +0200

    submit during creation

--- src/components/forms/CourseSuccess.tsx
+++ src/components/forms/CourseSuccess.tsx
@@ -8,34 +8,92 @@ import Button from '../button';
 
 import Images from '../../assets/images';
 import AccentColorButton from '../button/AccentColorButton';
+import { ApiContext } from '../../context/ApiContext';
+import { UserContext } from '../../context/UserContext';
 
 interface Props {
   course: CompletedCourse;
   subCourse: CompletedSubCourse;
   lectures: CompletedLecture[];
+  goBack: () => void;
 }
 
-const CourseSuccess: React.FC<Props> = (Props) => {
+const CourseSuccess: React.FC<Props> = ({
+  goBack,
+  course,
+  subCourse,
+  lectures,
+}) => {
   const history = useHistory();
+  const apiContext = React.useContext(ApiContext);
+  const userContext = React.useContext(UserContext);
+
+  const [published, setPublished] = React.useState(subCourse.published);
+
+  async function publishCourse() {
+    await apiContext.submitCourse(course.id, {
+      ...course,
+      submit: true,
+      instructors: [userContext.user!.id],
+    });
+    await apiContext.publishSubCourse(course.id, subCourse.id, {
+      ...subCourse,
+      instructors: [userContext.user!.id],
+    });
+    setPublished(true);
+  }
 
   return (
     <div className={classes.container}>
       <Images.Celebration width="400" height="400" />
-      <AccentColorButton
-        onClick={() => history.push(`/courses/${Props.course.id}`)}
-        accentColor="#4E6AE6"
-        label="Schließen"
-      />
-      <Button
-        onClick={() => {
-          window.location.reload();
-        }}
-        className={classes.button2}
-        color="#4E6AE6"
-        backgroundColor="white"
-      >
-        Weiteren Kurs erstellen
-      </Button>
+      {!published && lectures.length > 0 && (
+        <>
+          <div>
+            Dein Kurs wurde erstellt und kann bearbeitet werden. Soll er für
+            Schüler sichtbar sein?
+          </div>
+          <AccentColorButton
+            onClick={publishCourse}
+            accentColor="#4E6AE6"
+            label="Kurs veröffentlichen"
+          />
+          <AccentColorButton
+            onClick={() => history.push(`/courses/${course.id}`)}
+            accentColor="#GGGGGG"
+            label="Später veröffentlichen"
+          />
+        </>
+      )}
+      {!published && lectures.length === 0 && (
+        <>
+          <div>
+            Dein Kurs wurde erstellt und kann bearbeitet werden. Füge Termine
+            hinzu, um den Kurs veröffentlichen zu können
+          </div>
+          <AccentColorButton
+            onClick={goBack}
+            accentColor="#4E6AE6"
+            label="Termine hinzufügen"
+          />
+
+          <AccentColorButton
+            onClick={() => history.push(`/courses/${course.id}`)}
+            accentColor="#GGGGGG"
+            label="Später veröffentlichen"
+          />
+        </>
+      )}
+      {published && (
+        <div>
+          Dein Kurs ist für Schüler sichtbar, sobald er von Lern-Fair
+          freigegeben wurde.
+          <AccentColorButton
+            onClick={() => history.push(`/courses/${course.id}`)}
+            accentColor="#GGGGGG"
+            label="Schließen"
+          />
+        </div>
+      )}
     </div>
   );
 };
