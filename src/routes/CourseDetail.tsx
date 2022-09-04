69736c54d15872ab9cb7fc9d624a2f82100fe1c4
Jonas Wilms <JonasWilms2000@gmail.com>
2022-09-04 17:47:08 +0200

    linter hapiness

--- src/routes/CourseDetail.tsx
+++ src/routes/CourseDetail.tsx
@@ -311,18 +311,21 @@ const CourseDetail = (props: Props) => {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const url = (res as any).url as string;
         if (true /* url.includes("zoom") */) {
-          if (!confirm(
-            `Ich und meine Erziehungsberechtigten haben die Datenschutzbestimmungen (lern-fair.de/datenschutz) zur Kenntnis genommen und sind damit einverstanden,
-            dass meine persönlichen Daten für die Nutzung von Zoom verarbeitet werden.
-            Für die Durchführung des Kurses setzen wir Zoom ein.
-            Zoom hat seinen Sitz in den USA, was seinen Geheimdiensten und Behörden erlaubt auf jegliche Daten zuzugreifen,
-            damit potenziell auch auf Deine durch die Verwendung von Zoom anfallenden Daten.
-            Diese Erlaubnis, die die USA ihren Behörden gibt, ist nicht mit Europäischem Datenschutzrecht vereinbar.
-            Wir bemühen uns aber gemeinsam mit Zoom um den Schutz deiner Daten.
-            Mehr dazu kannst du in unserer Datenschutzerklärung nachlesen.
-            Wenn Du mit der Nutzung von Zoom nicht einverstanden bist oder sie dir (technisch) nicht möglich ist,
-            kontaktiere uns unter support@lern-fair.de`
-          )) {
+          if (
+            // eslint-disable-next-line
+            !window.confirm(
+              `Ich und meine Erziehungsberechtigten haben die Datenschutzbestimmungen (lern-fair.de/datenschutz) zur Kenntnis genommen und sind damit einverstanden,
+              dass meine persönlichen Daten für die Nutzung von Zoom verarbeitet werden.
+              Für die Durchführung des Kurses setzen wir Zoom ein.
+              Zoom hat seinen Sitz in den USA, was seinen Geheimdiensten und Behörden erlaubt auf jegliche Daten zuzugreifen,
+              damit potenziell auch auf Deine durch die Verwendung von Zoom anfallenden Daten.
+              Diese Erlaubnis, die die USA ihren Behörden gibt, ist nicht mit Europäischem Datenschutzrecht vereinbar.
+              Wir bemühen uns aber gemeinsam mit Zoom um den Schutz deiner Daten.
+              Mehr dazu kannst du in unserer Datenschutzerklärung nachlesen.
+              Wenn Du mit der Nutzung von Zoom nicht einverstanden bist oder sie dir (technisch) nicht möglich ist,
+              kontaktiere uns unter support@lern-fair.de`
+            )
+          ) {
             return;
           }
         }
